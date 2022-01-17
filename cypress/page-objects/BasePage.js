73d85a13c17ee416c6f9c22e75aeeb541175da8a
Guy Bitan <guy.bitan@gmail.com>
2022-01-17 13:42:25 +0200

    Add First Time

--- cypress/page-objects/BasePage.js
+++ cypress/page-objects/BasePage.js
@@ -1,25 +1,25 @@
 export default class BasePage {
-  static pause(ms) {
-    cy.wait(ms)
-  }
+    static pause(ms) {
+        cy.wait(ms)
+    }
 
-  static logInfo(message) {
-    cy.log(message)
-  }
+    static logInfo(message) {
+        cy.log(message)
+    }
 
-  static setMobileViewport() {
-    cy.viewport('iphone-x')
-  }
+    static setMobileViewport() {
+        cy.viewport('iphone-x')
+    }
 
-  static setTableViewport() {
-    cy.viewport('ipad-2')
-  }
+    static setTableViewport() {
+        cy.viewport('ipad-2')
+    }
 
-  static setDesktopViewport() {
-    cy.viewport('macbook-13')
-  }
+    static setDesktopViewport() {
+        cy.viewport('macbook-13')
+    }
 
-  static setLargeDesktopViewport() {
-    cy.viewport('macbook-15')
-  }
+    static setLargeDesktopViewport() {
+        cy.viewport('macbook-15')
+    }
 }
--- cypress/page-objects/components/Navbar.js
+++ cypress/page-objects/components/Navbar.js
@@ -1,13 +1,13 @@
 export default class Navbar {
-  static clickOnLogo() {
-    cy.get('.brand').click()
-  }
+    static clickOnLogo() {
+        cy.get('.brand').click()
+    }
 
-  static search(text) {
-    cy.get('#searchTerm').type(`${text} {enter}`)
-  }
+    static search(text) {
+        cy.get('#searchTerm').type(`${text} {enter}`)
+    }
 
-  static clickSignIn() {
-    cy.get('#signin_button').click()
-  }
+    static clickSignIn() {
+        cy.get('#signin_button').click()
+    }
 }
