c2d411cb0fec06f8afbedfe40b74bd56dd20bc97
Guy Bitan <guy.bitan@gmail.com>
2022-01-17 14:16:06 +0200

    Add First Time

--- cypress/integration/e2e/login.spec.js
+++ cypress/integration/e2e/login.spec.js
@@ -1,4 +1,4 @@
-import { url } from '../../../config'
+import { url, username, password } from '../../../config'
 import Navbar from '../../page-objects/components/Navbar'
 import LoginPage from '../../page-objects/pages/LoginPage'
 
@@ -24,6 +24,6 @@ describe('Login Success Test', () => {
 	})
 
 	it('should login into application', function () {
-		LoginPage.login('username', 'password')
+		LoginPage.login(username, password)
 	})
 })
